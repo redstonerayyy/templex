60c548d4db42f1dd0ce217f76df41ceadfefbdc7
Redstonerayy <antonpaulr@protonmail.com>
2023-04-13 18:30:41 +0200

    remove files

--- build/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const static_1 = require("./builders/static");
-const processed_1 = require("./builders/processed");
-const watchmode = process.argv[2] == "-W";
-console.log(watchmode);
-const publicdir = "./public";
-(0, static_1.make_static)("./static", publicdir);
-(0, processed_1.make_processed)("./processed", publicdir);
